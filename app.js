fa165dedc5a86c5da8e5f190ef106edcb7f2313b
nfahmed11 <nfahmed11@gmail.com>
2022-02-09 14:24:14 -0600

    final

---
+++ app.js
@@ -0,0 +1,41 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+ctx.strokeStyle = "black";
+ctx.lineJoin = "round";
+ctx.lineCap = "round";
+ctx.lineWidth = 20;
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+let hue = 0;
+
+function draw(e) {
+  if (!isDrawing) return;
+  console.log(e);
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+  ctx.beginPath();
+  //start from
+  ctx.moveTo(lastX, lastY);
+  //go to
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+  [lastX, lastY] = [e.offsetX, e.offestY];
+  hue++;
+  if (hue > 360) {
+    hue = 0;
+  }
+}
+
+canvas.addEventListener("mousedown", (e) => {
+  isDrawing = true;
+  [lastX, lastY] = [e.offsetX, e.offestY];
+});
+
+canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("mouseup", () => (isDrawing = false));
+canvas.addEventListener("mouseout", () => (isDrawing = false));
